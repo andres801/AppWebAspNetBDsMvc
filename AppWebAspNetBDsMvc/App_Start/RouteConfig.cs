57d551b8a095dd23a1501ae7cc93f0787ad2e190
Andres rdz hdz <sudafrica66@hotmail.com>
2021-10-03 21:41:35 -0500

    modificacion hecha en laptop, agregué el HelloWorldController e hice un agregado en RouteConfig

--- AppWebAspNetBDsMvc/App_Start/RouteConfig.cs
+++ AppWebAspNetBDsMvc/App_Start/RouteConfig.cs
@@ -18,6 +18,14 @@ namespace AppWebAspNetBDsMvc
                    url: "{controller}/{action}/{id}",
                    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
                );
+                   routes.MapRoute(
+                   name: "Hello",
+                   url: "{controller}/{action}/{name}/{id}"
+
+       // GET: /HelloWorld/Wilkommen/Scott/3
+
+       );
           }
+
      }
 }
---
+++ AppWebAspNetBDsMvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppWebAspNetBDsMvc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+          // 
+          // GET: /HelloWorld/ 
+
+          public string Index()
+          {
+               return "This is my <b>default</b> action...";
+          }
+
+          // 
+          // GET: /HelloWorld/Bienvenido/ 
+
+          public string Bienvenido()
+          {
+               return "This is the Bienvenido action method...";
+          }
+
+          // 
+          // GET: /HelloWorld/Welcome?name=Scott&numtimes=4 
+
+          public string Welcome(string name, int numTimes = 1)
+          {
+               return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+          }
+
+          // 
+          // GET: /HelloWorld/Wilkommen/1?name=Scott
+
+          public string Wilkommen(string name, int ID = 1)
+          {
+               return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+          }
+     }
+}
